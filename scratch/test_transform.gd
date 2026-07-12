e0268a2adddfba5aeef81858e27287896a85433c
Serge Svalov <wndrsw@gmail.com>
2026-07-12 06:51:35 +0300

    chore: remove unused configuration file and associated logic

--- scratch/test_transform.gd
+++
@@ -1,8 +0,0 @@
-extends SceneTree
-
-func _init():
-    var b = Basis(Vector3.UP, -PI/2)
-    print("Basis -PI/2: ", b)
-    var b2 = Basis(Vector3.UP, PI/2)
-    print("Basis PI/2: ", b2)
-    quit()
